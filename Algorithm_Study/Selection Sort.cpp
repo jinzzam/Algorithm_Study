081736e9a93e6427cc5478001de42cac4bb9d785
DESKTOP-Q4IFKLJ\jin <firstmind_@naver.com>
2018-03-09 02:30:32 +0900

    up

--- Algorithm_Study/Selection Sort.cpp
+++ Algorithm_Study/Selection Sort.cpp
@@ -1,23 +1,41 @@
 #include<stdio.h>
 #define N 11
 
-int main() {
-	int data[N] = {8, 31, 48, 73, 3, 65, 20, 29, 11, 15};
-
-	selectionSort(data, N);
-
-	for (int i = 0; i < N; i++)
-		printf("%d ", data[i]);
-}
-
-int selectionSort(int data[], int n) {
-	for (int i = N; i > N; i++) {
-		theLargest(data, i);
-
+int theLargest(int data[], int last) {	//가장 큰 수의 인덱스 반환
+	int largeIndex = last;
+	for (int i = 0; i < last; i++) {
+		if (data[last] > data[largeIndex]) {
+			largeIndex = last;
+		}
 	}
+	return largeIndex;
 }
 
-int theLargest(int data[], int n) {
+int selectionSort(int data[], int n) {	//선택정렬 실행
+	int last = n - 1;
+	int temp;
+	for (int i = last; i >= 0; i--) {
+		int k = theLargest(data, last);
+		temp = data[k];
+		data[k] = data[last];
+		data[last] = temp;
+		last--;
+	}
+	if (last == -1)
+		return 1;
+	else
+		return -1;
+}
 
+int main() {
+	int data[N] = { 8, 31, 48, 73, 3, 65, 20, 29, 11, 15 };
 
+	if (selectionSort(data, N) == 1) {
+		for (int i = 0; i < N; i++)
+			printf("%d ", data[i]);
+	}
+	else {
+		printf("ERROR IN SORTING!!");
+	}
+	return 0;
 }
